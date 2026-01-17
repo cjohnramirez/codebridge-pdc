937f10dce849be71e402a40c3e1cf6b0ebf82a7e
SuiSensei <jcramirez340@gmail.com>
2026-01-17 01:34:35 +0000

    (feat) main structure

---
+++ 1 - Calculator/main.py
@@ -0,0 +1,22 @@
+def get_number():
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+    return x, y 
+    
+def game():
+    while True: 
+        while True:
+            choice_array = [1, 2, 3, 4, 5]  
+            
+            print('1: Addition \n'
+            '2: Subtraction \n'
+            '3: Multiplication \n'
+            '4: Division \n'
+            '5: Exit \n')
+            
+            # convert input string to int first
+            choice = int(input("Enter your user input: "))
+            if (choice in choice_array):
+                break
+    
+game()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # codebridge-pdc
 A unified Codespace repository for the subject CS323 - Parallel and Distributed Computing
+
+# Documentation
+## 1st Meeting - 1/17/2026
