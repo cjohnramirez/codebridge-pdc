f4982384502f838fd449ccfacc711f4abec25547
Gerlie Campion <Campiongerlie18@gmail.com>
2026-04-11 12:09:39 +0800

    Distribute Orders to Workers

--- 5 - Distributed/mpi_order_processing.py
+++ 5 - Distributed/mpi_order_processing.py
@@ -27,3 +27,14 @@ if rank == 0:
     print("MASTER: Created orders:")
     for o in orders:
         print(o)
+        print("\nMASTER: Distributing orders...\n")
+
+    # Send orders to workers
+    for i, order in enumerate(orders):
+        worker_rank = (i % (size - 1)) + 1
+        comm.send(order, dest=worker_rank, tag=0)
+        print(f"MASTER: Sent order {order['id']} to worker {worker_rank}")
+
+    # Send stop signals
+    for i in range(1, size):
+        comm.send(None, dest=i, tag=0)
